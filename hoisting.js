1a27e464273af29afd6126b13db5f78c56bd8cb4
Carlos Mendoza <mendoza1990carlos@gmail.com>
2017-10-13 01:59:53 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -7,14 +7,12 @@ function callMe() {
   var lyric = "maybe";
 }
 
-
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -24,7 +22,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+
+      console.log(name);
   }
 }
